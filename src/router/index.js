62c1efff82e7abf93a375ae0f81efeda3bd855dc
biwen.lai <biwen.lai@qizhidao.com>
2023-08-19 10:33:19 +0800

    feat: 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from "vue-router";
 const Home = () => import("@/pages/index.vue");
 const HelloWorld = () => import("@/components/HelloWorld.vue");
 const User = () => import("@/pages/user.vue");
+const NotFound = () => import("@/pages/NotFound.vue");
 
 const routes = [
   { path: "/", component: Home },
   { path: "/HelloWorld", component: HelloWorld },
   { path: "/users/:id", component: User },
+  { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
 ];
 
 const router = createRouter({
