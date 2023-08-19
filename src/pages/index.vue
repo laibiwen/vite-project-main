9636d1e9dacef64a0bd60320110150a779f65256
biwen.lai <biwen.lai@qizhidao.com>
2023-08-19 11:08:30 +0800

    feat: router-link

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,31 @@
 <template>
   <div>首页</div>
+  <ul>
+    <router-link v-for="item in linkList" :to="item.path" class="link">{{
+      item.name
+    }}</router-link>
+  </ul>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+const linkList = [
+  {
+    path: "/HelloWorld",
+    name: "HelloWorld",
+  },
+  {
+    path: "/users/d99d9d/profile",
+    name: "profile",
+  },
+  {
+    path: "/users/d99d9d/posts",
+    name: "posts",
+  },
+];
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.link {
+  margin-right: 12px;
+}
+</style>
